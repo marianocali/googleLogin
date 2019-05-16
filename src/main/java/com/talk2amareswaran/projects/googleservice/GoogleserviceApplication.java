36a0f3f6063f6e377fb58b43fa09b8af83fc7109
Mariano Cali <mariano.cali@globant.com>
2019-05-16 18:13:48 -0300

    uploading project google login

---
+++ src/main/java/com/talk2amareswaran/projects/googleservice/GoogleserviceApplication.java
@@ -0,0 +1,28 @@
+package com.talk2amareswaran.projects.googleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@SpringBootApplication
+@RestController
+public class GoogleserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleserviceApplication.class, args);
+	}
+	
+	@RequestMapping(value="/firstapp")
+	public String firstApp() {
+		return "My Google Service is working fine in GCP Environment";
+	}
+
+	@RequestMapping(value="/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+
+}
